a7fa3c41f5e9e0f5052a8d5c43f80abaf0d6fd46
Troy Kollar <troykollar@gmail.com>
2020-02-06 17:50:16 -0500

    Finished code for seven segment display driver

--- FPGA/greenhouse.v
+++ FPGA/greenhouse.v
@@ -1,11 +1,24 @@
 module greenhouse(input clk, 
                   input gpio_0,
                   output wire [6:0] disp_segs);
-reg [7:0] value;
 
+reg [24:0] counter = 0;
+reg [3:0] value = 0;
+
+always @(posedge clk)
+    if (counter == 25'd25_000_000)  counter <= 0;
+    else    counter <= counter + 1;
+
+always @(posedge clk)
+    if (value == 4'd10) value <= 0;
+    else if (counter == 25'd25_000_000) value <= value + 1;
+    else    value <= value;
+
+/*
 always @(posedge clk)
     if (gpio_0)     value <= 8'd1;
     else            value <= 8'd0;
+*/
 
 val_to_seven_seg disp(clk, value, disp_segs);
 
--- FPGA/val_to_seven_seg.v
+++ FPGA/val_to_seven_seg.v
@@ -6,7 +6,14 @@ module val_to_seven_seg(input wire clk,
 always @(posedge clk)
     if      (value == 8'd0)     display_segs <= 7'b1000000;
     else if (value == 8'd1)     display_segs <= 7'b1111001;
-    else if (value == 8'd2)     display_segs <= 7'b1011011;
+    else if (value == 8'd2)     display_segs <= 7'b0100100;
+    else if (value == 8'd3)     display_segs <= 7'b0110000;
+    else if (value == 8'd4)     display_segs <= 7'b0011001;
+    else if (value == 8'd5)     display_segs <= 7'b0010010;
+    else if (value == 8'd6)     display_segs <= 7'b0000010;
+    else if (value == 8'd7)     display_segs <= 7'b1111000;
+    else if (value == 8'd8)     display_segs <= 7'b0000000;
+    else if (value == 8'd9)     display_segs <= 7'b0010000;
     else                        display_segs <= 7'b0000000;
 
 endmodule
