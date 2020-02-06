1225c0e8d0073597a9d667f553a25ae2c9fe9d51
Troy Kollar <troykollar@gmail.com>
2020-02-06 17:09:09 -0500

    Got seven segment display working

--- FPGA/val_to_seven_seg.v
+++ FPGA/val_to_seven_seg.v
@@ -4,9 +4,9 @@ module val_to_seven_seg(input wire clk,
                         );
 
 always @(posedge clk)
-    if      (value == 8'd0)     display_segs <= 7'b0000001;
-    else if (value == 8'd1)     display_segs <= 7'b0110000;
-    else if (value == 8'd2)     display_segs <= 7'b1101101;
+    if      (value == 8'd0)     display_segs <= 7'b1000000;
+    else if (value == 8'd1)     display_segs <= 7'b1111001;
+    else if (value == 8'd2)     display_segs <= 7'b1011011;
     else                        display_segs <= 7'b0000000;
 
 endmodule
