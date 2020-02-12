bbed0fc864bd8392af97f9ad61dd1906d8fd2ddb
PhillMyCup <PhillMyCup@users.noreply.github.com>
2020-02-12 09:15:05 -0500

    Update Lighting.ino

--- Lighting/Lighting.ino
+++ Lighting/Lighting.ino
@@ -1,14 +1,14 @@
 int Power;
 void setup() {
 pinMode(12, OUTPUT);
-//Serial.begin(9600);
+Serial.begin(9600);
 }
 
 void loop() {
     Power=analogRead(A0);
-    //Serial.print(Power);
-    //Serial.print("   ");
-    if (Power < 500){
+    Serial.print(Power);
+    Serial.print("   ");
+    if (Power < 700){
       digitalWrite(12, HIGH);
       delay(2000);
     }
