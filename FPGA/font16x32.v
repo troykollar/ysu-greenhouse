4dda693bd1c9554260888e70559363b07e3a034f
Troy Kollar <troykollar@gmail.com>
2020-02-25 13:51:19 -0500

    Got zero and one working

--- FPGA/font16x32.v
+++ FPGA/font16x32.v
@@ -61,6 +61,42 @@ module font16x32(
                     5'd31   :    character_x = 16'b0000000000000000;
                     default:     character_x = 16'b0000000000000000;
                 endcase
+            8'd1    :
+                case (pixel_y)
+                    5'd0    :    character_x = 16'b0000000000000000;
+                    5'd1    :    character_x = 16'b0000000000000000;
+                    5'd2    :    character_x = 16'b0000000000000000;
+                    5'd3    :    character_x = 16'b0000000000000000;
+                    5'd4    :    character_x = 16'b0000000110000000;
+                    5'd5    :    character_x = 16'b0000001110000000;
+                    5'd6    :    character_x = 16'b0000011110000000;
+                    5'd7    :    character_x = 16'b0000111110000000;
+                    5'd8    :    character_x = 16'b0000000110000000;
+                    5'd9    :    character_x = 16'b0000000110000000;
+                    5'd10   :    character_x = 16'b0000000110000000;
+                    5'd11   :    character_x = 16'b0000000110000000;
+                    5'd12   :    character_x = 16'b0000000110000000;
+                    5'd13   :    character_x = 16'b0000000110000000;
+                    5'd14   :    character_x = 16'b0000000110000000;
+                    5'd15   :    character_x = 16'b0000000110000000;
+                    5'd16   :    character_x = 16'b0000000110000000;
+                    5'd17   :    character_x = 16'b0000000110000000;
+                    5'd18   :    character_x = 16'b0000000110000000;
+                    5'd19   :    character_x = 16'b0000000110000000;
+                    5'd20   :    character_x = 16'b0000000110000000;
+                    5'd21   :    character_x = 16'b0000000110000000;
+                    5'd22   :    character_x = 16'b0000000110000000;
+                    5'd23   :    character_x = 16'b0000000110000000;
+                    5'd24   :    character_x = 16'b0000000110000000;
+                    5'd25   :    character_x = 16'b0000000110000000;
+                    5'd26   :    character_x = 16'b0011111111111100;
+                    5'd27   :    character_x = 16'b0011111111111100;
+                    5'd28   :    character_x = 16'b0000000000000000;
+                    5'd29   :    character_x = 16'b0000000000000000;
+                    5'd30   :    character_x = 16'b0000000000000000;
+                    5'd31   :    character_x = 16'b0000000000000000;
+                    default:     character_x = 16'b0000000000000000;
+                endcase
         endcase
 
         wire in_char_tile_x, in_char_tile_y, in_char_tile;
