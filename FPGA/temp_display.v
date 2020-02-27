538a534c60ff7f175eafd279b1e571ba05f17b15
Troy Kollar <troykollar@gmail.com>
2020-02-26 19:04:59 -0500

    Rearranged order so values display correctly (hundreds on left, ones on right etc.)

--- FPGA/temp_display.v
+++ FPGA/temp_display.v
@@ -11,14 +11,14 @@ module temp_display #(
     output wire on_temp_display
 );
 
-    parameter digit_0_x1 = x1;
-    parameter digit_1_x1 = digit_0_x1 + 16;
-    parameter digit_2_x1 = digit_1_x1 + 16;
+    parameter digit_0_x1 = digit_1_x1 + 16;
+    parameter digit_1_x1 = digit_2_x1 + 16;
+    parameter digit_2_x1 = x1;
 
     wire on_hundreds, on_tens, on_ones;
     assign on_temp_display = (on_hundreds || on_tens || on_ones);
 
-    font16x32 #(.x1(digit_0_x1), .y1(50)) hundreds_place(
+    font16x32 #(.x1(digit_2_x1), .y1(50)) hundreds_place(
         .character_code(temp_value_100),
         .x(x),
         .y(y),
@@ -32,7 +32,7 @@ module temp_display #(
         .on_char(on_tens)
     );
 
-    font16x32 #(.x1(x1), .y1(50)) ones_place(
+    font16x32 #(.x1(digit_0_x1), .y1(50)) ones_place(
         .character_code(temp_value_1),
         .x(x),
         .y(y),
