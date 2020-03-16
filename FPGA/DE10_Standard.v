d7cfa323048375ea64160d023e0e8fc1a154de05
Troy Kollar <troykollar@gmail.com>
2020-03-16 15:50:08 -0400

    Changed set temp and humidity to their own indepented registers

--- FPGA/DE10_Standard.v
+++ FPGA/DE10_Standard.v
@@ -58,21 +58,58 @@ module DE10_Standard(
 	wire [11:0] adc_values [7:0];
 	wire [10:0] temp_c;
 	wire [11:0] temp_f;
-	assign LEDR = adc_values [0] [11:1];
+	reg [11:0] SET_TEMP_F = 12'd72;
+	reg [7:0] SET_HUM = 8'd50;
+	wire [1:0] temp_adjust;
+	wire [1:0] hum_adjust;
+	wire [2:0] time_adjust;
+	wire [3:0] menu_state;
+	assign LEDR[3:0] = menu_state;
 
 //=======================================================
 //  Structural coding
 //=======================================================
 
+	menu menu_sm(
+		.clk(CLOCK_50),
+		.up(KEY[2]),
+		.down(KEY[3]),
+		.left(KEY[1]),
+		.right(KEY[0]),
+		.state(menu_state),
+		.temp_adjust(temp_adjust),
+		.hum_adjust(hum_adjust),
+		.time_adjust(time_adjust)
+	);
+
+	always @(posedge CLOCK_50)
+		if (temp_adjust == 2'd1)
+			SET_TEMP_F <= SET_TEMP_F + 1;
+		else if (temp_adjust == 2'd2)
+			SET_TEMP_F <= SET_TEMP_F - 1;
+		else
+			SET_TEMP_F <= SET_TEMP_F;
+
+	always @(posedge CLOCK_50)
+		if (hum_adjust == 2'd1)
+			SET_HUM <= SET_HUM + 1;
+		else if (hum_adjust == 2'd2)
+			SET_HUM <= SET_HUM - 1;
+		else
+			SET_HUM <= SET_HUM;
+
 display_controller display(
 	.CLOCK_50(CLOCK_50),
 	.TEMP_F({2'b00, SW}),
-	.SET_TEMP_F({2'b00, SW}),
+	.SET_TEMP_F(SET_TEMP_F),
 	.TEMP_STATUS(SW[1:0]),
 	.HUM(SW[7:0]),
-	.SET_HUM(SW[7:0]),
+	.SET_HUM(SET_HUM),
 	.HUM_STATUS(SW[1:0]),
 	.SW(SW),
+	.TIME_HOURS(hours),
+	.TIME_MINUTES(minutes),
+	.MENU_STATE(menu_state),
 	.VGA_BLANK_N(VGA_BLANK_N),
 	.VGA_CLK(VGA_CLK),
 	.VGA_R(VGA_R),
@@ -106,24 +143,55 @@ voltage_to_temp v_to_temp (
 	.temp_f_signed(temp_f)
 );
 
+wire [5:0] seconds;
+wire [5:0] minutes;
+wire [4:0] hours;
+timekeeper time_keeper(
+	.clk(CLOCK_50),
+	.time_adjust(time_adjust),
+	.seconds(seconds),
+	.minutes(minutes),
+	.hours(hours)
+);
+
+wire [7:0] seconds_bcd, minutes_bcd, hours_bcd;
+bin2bcd timebcd(seconds, seconds_bcd);
 val_to_seven_seg hex0 (
 	.clk (CLOCK_50),
-	.value (temp_f[3:0]),
+	.value (seconds_bcd[3:0]),
 	.display_segs (HEX0)
 );
 
 val_to_seven_seg hex1 (
 	.clk (CLOCK_50),
-	.value (temp_f[7:4]),
+	.value (seconds_bcd[7:4]),
 	.display_segs (HEX1)
 );
 
+bin2bcd(minutes, minutes_bcd);
 val_to_seven_seg hex2 (
 	.clk (CLOCK_50),
-	.value ({1'b0, temp_f[10:8]}),
+	.value (minutes_bcd[3:0]),
 	.display_segs (HEX2)
 );
 
+val_to_seven_seg hex3 (
+	.clk (CLOCK_50),
+	.value (minutes_bcd[7:4]),
+	.display_segs (HEX3)
+);
 
+bin2bcd(hours, hours_bcd);
+val_to_seven_seg hex4 (
+	.clk (CLOCK_50),
+	.value (hours_bcd[3:0]),
+	.display_segs (HEX4)
+);
+
+val_to_seven_seg hex5 (
+	.clk (CLOCK_50),
+	.value (hours_bcd[7:4]),
+	.display_segs (HEX5)
+);
 
 endmodule
